3e94cfd1d3cb1df04f81ff87f0cd01ddfc3ca7da
Will sumfest <was022@berkeley.edu>
2016-09-26 00:54:48 -0700

    configure rspec for testing suite, added random string generater

--- lib/quick_export.rb
+++ lib/quick_export.rb
@@ -1,5 +1,13 @@
 require "quick_export/version"
 
 module QuickExport
+	class Base 
 
+		def self.generate_random_filename
+			o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+			str_prefix = (0...30).map { o[rand(o.length)] }.join
+			str_prefix
+		end
+
+	end
 end
