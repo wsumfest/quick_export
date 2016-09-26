d9ee58ddaeac3b4b12ab240b7298a7c895752f30
Will sumfest <was022@berkeley.edu>
2016-09-26 16:17:45 -0700

    tested inheritance of subclasses, added comments

--- lib/quick_export.rb
+++ lib/quick_export.rb
@@ -2,14 +2,25 @@ require "quick_export/version"
 require "active_support"
 
 module QuickExport
-	extend ActiveSupport::Concern
-	class Base 
-
-		def self.generate_random_filename
-			o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
-			str_prefix = (0...30).map { o[rand(o.length)] }.join
-			str_prefix
-		end
-
+  extend ActiveSupport::Concern
+  class Base
+  	"""
+  	Generates a string of random letters for filenames.
+  	""" 
+    def self.generate_random_filename
+	  o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+	  (0...30).map { o[rand(o.length)] }.join
 	end
+
+  end
+  class To_CSV < Base
+	@@file_extension = ".csv"
+	def self.dump_csv
+	end
+  end
+  class To_XLS < Base
+	@@file_extension = ".xls"
+	def self.dump_xls
+	end
+  end
 end
--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -11,5 +11,15 @@ describe QuickExport::Base do
 			second_filename = QuickExport::Base.generate_random_filename
 			expect(first_filename).not_to eq(second_filename)
 		end
+		it "should test the inheritance of To_CSV class" do
+			filename = QuickExport::To_CSV.generate_random_filename
+			expect(filename).not_to be(nil)
+			expect(filename.size).to eq(30)
+		end
+		it "should test the inheritance of To_XLS class" do
+			filename = QuickExport::To_XLS.generate_random_filename
+			expect(filename).not_to be(nil)
+			expect(filename.size).to eq(30)
+		end
 	end	
 end
