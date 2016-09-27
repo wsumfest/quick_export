045de17bc0c0765e69f1ca32ad1619c2b79a119a
Will sumfest <was022@berkeley.edu>
2016-09-26 19:02:51 -0700

    added comments to methods

--- lib/quick_export.rb
+++ lib/quick_export.rb
@@ -13,32 +13,48 @@ module QuickExport
 	  (0...30).map { o[rand(o.length)] }.join
 	end
 
+	"""
+	Getter method for file extension.
+	"""
 	def self.get_file_extension
 		return @file_extension
 	end
 
+	"""
+	Full filename is not implemented in Base class.
+	"""
 	def self.build_filename
 
 	end
 
   end
+
   class To_CSV < Base
 	@file_extension = ".csv"
 	def self.dump_csv
 	end
 
+	"""
+	Returns a random filename with a csv extension.
+	"""
 	def self.build_filename
 		generate_random_filename + @file_extension
 	end
 
   end
+
   class To_XLS < Base
 	@file_extension = ".xls"
 	def self.dump_xls
 	end
 
+	"""
+	Returns a random filename with a xls extension.
+	"""
 	def self.build_filename
 		generate_random_filename + @file_extension
 	end
+	
   end
+
 end
